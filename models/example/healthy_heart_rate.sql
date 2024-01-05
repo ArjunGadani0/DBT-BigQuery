588171ca0413d11f95bfa1963f4ccb398d7c504b
pim obe <pimobe2343@aseall.com>
2024-01-05 06:14:47 +0000

    Demo

--- models/example/healthy_heart_rate.sql
+++ models/example/healthy_heart_rate.sql
@@ -2,7 +2,7 @@
 
 with HeartRateCte as (
     SELECT * 
-    FROM `playground-s-11-047fc222.myDataset.watch_data`
+    FROM `playground-s-11-dcb28ab8.myDataset.watch_data`
     where AvgHeartRate < 90
 )
 SELECT * FROM HeartRateCte
